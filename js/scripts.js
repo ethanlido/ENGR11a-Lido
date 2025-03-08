cada521630583dd0c67c01e8a6aa956415229ce9
ethanlido <ethanlido@brandeis.edu>
2025-03-07 23:31:22 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -22,18 +22,14 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-
-  // Loop around if n exceeds number of slides
   if (n > slides.length) { slideIndex = 1 }
   if (n < 1) { slideIndex = slides.length }
 
-  // Hide all slides by setting opacity to 0
   for (i = 0; i < slides.length; i++) {
     slides[i].style.opacity = "0";  // Hide the slide
     slides[i].classList.remove("active");  // Remove active class
   }
 
-  // Remove active class from all dots
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
