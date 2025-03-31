f48186184d82013b798551a8550ef52ab9628a20
ethanlido <ethanlido@brandeis.edu>
2025-03-31 16:07:02 -0400

    debugging sidebar

--- js/scripts.js
+++ js/scripts.js
@@ -52,6 +52,25 @@ function autoSlide() {
 
 setInterval(autoSlide, 10000); // Change image every 3 seconds
 
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        // Uncomment Below to persist sidebar toggle between refreshes
+        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+        //     document.body.classList.toggle('sb-sidenav-toggled');
+        // }
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+
+});
+
+
 
 /*!
 let slideIndex = 1;
